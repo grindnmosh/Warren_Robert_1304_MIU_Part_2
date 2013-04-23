1c2f17fd034d82537f159d3d9fb4048f03bee780
Robert Warren <grindnmosh@gmail.com>
2013-04-22 18:02:21 -0700

    added view page. it appears blank till you scroll down. I am not sure what is causing this bug. fixed java script to support this.

--- gold/js/Sample-main.js
+++ gold/js/Sample-main.js
@@ -55,7 +55,7 @@ function createButtons(key, buttons) {
 	editButton.setAttribute("type", "button");
 	editButton.setAttribute("value", "Edit Bill");
 	editButton.setAttribute("id", "editBill");
-	editButton.href = "#";
+	editButton.href = "#add";
 	editButton.key = key;
 	editButton.addEventListener("click", makeEdits);
 	buttons.appendChild(editButton);
@@ -73,36 +73,41 @@ function createButtons(key, buttons) {
 	buttons.appendChild(delButton);
 };
 
+function editAdd(){
+        window.location='#add';
+    }
+    
 function makeEdits() {
 	var value = localStorage.getItem(this.key);
 	var recallData = JSON.parse(value);
-	switchPages("off");
-	displaySavedInfo("btype").value = recallData.btype[1];
-	displaySavedInfo("bname").value = recallData.bname[1];
-	displaySavedInfo("amt").value = recallData.amt[1];
-	displaySavedInfo("prio").value = recallData.prio[1];
-	displaySavedInfo("due").value = recallData.due[1];
-	displaySavedInfo("freqs").value = recallData.freqs[1];
+	//switchPages("off");
+	editAdd();
+	document.getElementById("btype").value = recallData.btype[1];
+	document.getElementById("bname").value = recallData.bname[1];
+	document.getElementById("amt").value = recallData.amt[1];
+	document.getElementById("prio").value = recallData.prio[1];
+	document.getElementById("due").value = recallData.due[1];
+	document.getElementById("freqs").value = recallData.freqs[1];
 	var radioSelected = document.forms[0].status;
 	for(i=0; i<radioSelected.length; i++) {
 		if(radioSelected[i].value == recallData.pd[1]) {
 			radioSelected[i].setAttribute("checked", "checked");
 		};
 	};
-	displaySavedInfo("pdwith").value = recallData.pdwith[1];    	
+	document.getElementById("pdwith").value = recallData.pdwith[1];    	
 	if(recallData.ontime[1] == "On Time") {
-    	displaySavedInfo("ontime").setAttribute("checked", "checked");
+    	document.getElementById("ontime").setAttribute("checked", "checked");
 	};
 	if(recallData.late[1] == "Late") {
-    	displaySavedInfo("late").setAttribute("checked", "checked");
+    	document.getElementById("late").setAttribute("checked", "checked");
 	};
 	if(recallData.lfee[1] == "Late Fee") {
-    	displaySavedInfo("lfee").setAttribute("checked", "checked");
+    	document.getElementById("lfee").setAttribute("checked", "checked");
 	};
-	displaySavedInfo("textArea").value = recallData.textArea[1];
+	document.getElementById("textArea").value = recallData.textArea[1];
 	//saveBill.removeEventListener("click", validate);
-	displaySavedInfo("saveMe").value = "Edit Bill";
-	var changeSubmit = displaySavedInfo("saveMe");
+	document.getElementById("saveMe").value = "Edit Bill";
+	var changeSubmit = document.getElementById("saveMe");
 	changeSubmit.addEventListener("click");
 	changeSubmit.key = this.key
 };
@@ -127,14 +132,6 @@ function cleanHouse() {
 	};
 	return false;
 };
-
-
-
-function displaySavedInfo(field) {
-	var theBill = document.getElementById(field);
-	return theBill
-};
-
 
 function cleanHouse() {
 	if(localStorage.length === 0) {
@@ -169,48 +166,27 @@ function getSelectedRadio() {
 
 	
 function getCheckBoxOnTime() {
-	 if(displaySavedInfo('ontime').checked){
-		 onTime = displaySavedInfo('ontime').value;
+	 if(document.getElementById('ontime').checked){
+		 onTime = document.getElementById('ontime').value;
 	 }else{
 		 onTime = "N/A";
 	 }
 };
 
 function getCheckBoxLate() {
-	 if(displaySavedInfo('late').checked){
-		 late = displaySavedInfo('late').value;
+	 if(document.getElementById('late').checked){
+		 late = document.getElementById('late').value;
 	 }else{
 		 late = "N/A";
 	 }
 };
 
 function getCheckBoxLateFee() {
-	 if(displaySavedInfo('lfee').checked){
-		 lateFee = displaySavedInfo('lfee').value;
+	 if(document.getElementById('lfee').checked){
+		 lateFee = document.getElementById('lfee').value;
 	 }else{
 		 lateFee = "N/A";
 	 }
-};
-
-function switchPages(status) {
-	switch(status) {
-		case "on":
-			displaySavedInfo("addBill").style.display = "none";
-			displaySavedInfo("clearAllData").style.display = "block";
-			displaySavedInfo("displayLink").style.display = "none";
-			displaySavedInfo("addNewBill").style.display = "block";
-			displaySavedInfo("footer").style.display = "none";
-			break;
-		case "off":
-			displaySavedInfo("addBill").style.display = "block";
-			displaySavedInfo("clearAllData").style.display = "block";
-			displaySavedInfo("displayLink").style.display = "block";
-			displaySavedInfo("addNewBill").style.display = "none";
-			displaySavedInfo("bill").style.display = "none";
-			break;
-		default:
-			return false;
-	};
 };
 
 //var autofillData = function (){};
@@ -223,7 +199,8 @@ function getSampleBills() {
 
 //var getData = function(){};
 function getBill() {
-	switchPages("on"); 
+	//switchPages("on"); 
+	window.location='#view';
 	if(localStorage.length === 0) {
 		getSampleBills();
 		alert("There is no data to view. Sample Data has been added.");
@@ -233,7 +210,9 @@ function getBill() {
 	var newList = document.createElement("ul");
 	newDiv.appendChild(newList);	
 	document.body.appendChild(newDiv);
-	displaySavedInfo("bill").style.display = "block";
+	var newList = document.createElement("ul");
+	newDiv.appendChild(newList);	
+	document.getElementById("display").style.display = "block";
 	for(i=0, l=localStorage.length; i<l; i++) {
 		var newItem = document.createElement("li");
 		var buttons = document.createElement("li");
@@ -270,18 +249,18 @@ function getForm(key) {
 	getCheckBoxLate();
 	getCheckBoxLateFee();
 	var item = {};
-			item.btype = ["Bill Type: ", displaySavedInfo("btype").value];
-		item.bname = ["Bill Name: ", displaySavedInfo("bname").value];
-		item.prio = ["Bill Priority: ", displaySavedInfo("prio").value];
-		item.amt = ["Bill Amount: $", displaySavedInfo("amt").value];
-		 	item.due = ["Bill Due Date: ", displaySavedInfo("due").value];
-		 	item.freqs = ["Bill Frequency: ", displaySavedInfo("freqs").value];
+			item.btype = ["Bill Type: ", document.getElementById("btype").value];
+		item.bname = ["Bill Name: ", document.getElementById("bname").value];
+		item.prio = ["Bill Priority: ", document.getElementById("prio").value];
+		item.amt = ["Bill Amount: $", document.getElementById("amt").value];
+		 	item.due = ["Bill Due Date: ", document.getElementById("due").value];
+		 	item.freqs = ["Bill Frequency: ", document.getElementById("freqs").value];
 		item.pd = ["Paid: ", paidValue];
 		 	item.pdwith = ["Paid with: ", paymentValue];
 		 	item.ontime = ["On time?: ", onTime];   		 	
 		 	item.late = ["Late?: ", late];
 		 	item.lfee = ["Late Fee?: ", lateFee];
-		 	item.textArea = ["Comments: ", displaySavedInfo("textArea").value];
+		 	item.textArea = ["Comments: ", document.getElementById("textArea").value];
 	localStorage.setItem(id, JSON.stringify(item));
 	alert(saveSuccess);
 	 	saveMe.setAttribute("type", "reset");  
